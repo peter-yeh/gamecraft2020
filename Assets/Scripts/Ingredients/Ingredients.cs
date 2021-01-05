c244c37e3807429683ff21a201e9725686725636
Yeh Yu Chun <yehyuchun2@gmail.com>
2021-01-05 14:22:40 +0000

    revert my changes for destoying objects Everything should work as normal now

--- Assets/Scripts/Ingredients/Ingredients.cs
+++ Assets/Scripts/Ingredients/Ingredients.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Ingredients : MonoBehaviour
 {
     [SerializeField] private float animateRate = 0.5f;
+    private Rigidbody2D rb;
 
     private void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
         StartCoroutine(Animate());
     }
 
@@ -26,4 +28,34 @@ public class Ingredients : MonoBehaviour
 
         }
     }
+
+    //public void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (!col.gameObject.CompareTag("Player"))
+    //    {
+    //        StartCoroutine(FlashWhite());
+    //    }
+    //}
+
+    //private IEnumerator FlashWhite()
+    //{
+    //    WaitForSeconds wait = new WaitForSeconds(0.5f);
+    //    SpriteRenderer sprite = transform.GetComponent<SpriteRenderer>();
+    //    Debug.Log("Running FlashWhite");
+
+    //    // set to flash twice
+    //    for (int i = 0; i < 2; i++)
+    //    {
+    //        yield return wait;
+    //        sprite.color = Color.white;
+
+    //        yield return wait;
+    //        sprite.color = Color.clear;
+
+    //    }
+
+    //    //Destroy(transform);
+    //}
+
+
 }
--- Assets/Scripts/Level/ItemCollision.cs
+++ Assets/Scripts/Level/ItemCollision.cs
@@ -1,43 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ItemCollision : MonoBehaviour
 {
-    private SpriteRenderer sprite;
-    private Color defaultColor;
 
     // Collision with ingredients and bombs
     void OnTriggerEnter2D(Collider2D col)
     {
         GameObject go = col.gameObject;
-
-        //if (go.GetComponent<Ingredients>() != null)
-        //{
-        //    StartCoroutine(FlashWhite(go));
-
-        //}
-        //Destroy(go);
-
-
+        Destroy(go);
     }
 
-    //private IEnumerator FlashWhite(GameObject go)
-    //{
-    //    WaitForSeconds wait = new WaitForSeconds(0.5f);
-    //    SpriteRenderer sprite = go.GetComponent<SpriteRenderer>();
-    //    Debug.Log("Running FlashWhite");
-
-    //    // set to flash twice
-    //    for (int i = 0; i < 2; i++)
-    //    {
-    //        yield return wait;
-    //        sprite.color = Color.white;
-
-    //        yield return wait;
-    //        sprite.color = Color.clear;
-
-    //    }
-    //    Destroy(go);
-    //}
 }
