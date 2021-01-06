22702f2410b1832b42e469162c4a2dd52162cd9c
Zhu Yuxuan <e0424709@u.nus.edu>
2021-01-06 15:24:11 +0000

    Add ingredients needed for each food(lvl 2 and 3 unlocked for testing)

---
+++ Assets/Scripts/UI/RecipeImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RecipeImage : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+{
+    [SerializeField] GameObject recipe;
+
+    private Image image;
+    private Color imageColor;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        imageColor = image.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.color = new Color32(255, 255, 255, 1);
+        recipe.SetActive(true);
+        Debug.Log("Mouse Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.color = imageColor;
+        recipe.SetActive(false);
+        Debug.Log("Mouse Exit");
+    }
+}
