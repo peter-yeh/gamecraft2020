7049ee9d1604ed58a398a286f97e76b78a686c3e
Zhu Yuxuan <e0424709@u.nus.edu>
2021-01-06 03:24:05 +0000

    Add countdownTimer

---
+++ Assets/Scripts/Level/CountdownTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float currentTime;
+    private float startingTime = 60f;
+
+    [SerializeField] TextMeshProUGUI countdownText;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        if (currentTime == 60)
+        {
+            countdownText.text = "01:00";
+            return;
+        }
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
+        countdownText.text = "00:" + currentTime.ToString("0");
+    }
+}
