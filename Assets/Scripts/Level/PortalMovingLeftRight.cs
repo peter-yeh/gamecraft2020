3e982eca57a15abc28538444df9e234a3484ddad
Robin Ho <rhlb98@gmail.com>
2021-01-06 11:30:54 +0000

    Created an alternative (probably more fun) version of the bonus level

---
+++ Assets/Scripts/Level/PortalMovingLeftRight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMovingLeftRight : MonoBehaviour
+{
+    public float rightLimit;
+    public float leftLimit;
+    public float speed;
+    private int direction = 1;
+
+    void Update()
+    {
+        if (transform.position.x > rightLimit)
+        {
+            direction = -1;
+        }
+        else if (transform.position.x < leftLimit)
+        {
+            direction = 1;
+        }
+
+        transform.position = new Vector2(transform.position.x + speed * direction * Time.deltaTime, transform.position.y);
+    }
+}
---
+++ Assets/Scripts/Level/PushDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushDown : MonoBehaviour
+{
+    public bool pushRight;
+    public float thrust;
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            if (pushRight)
+            {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.right * thrust);
+            } else
+            {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(-1* transform.right * thrust);
+            }
+
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(-1* transform.up * thrust);
+        }
+    }
+}
