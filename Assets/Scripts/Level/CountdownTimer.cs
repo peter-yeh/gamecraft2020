d65f8cec55fdc12b19a1530147785b91c3302ab3
Zhu Yuxuan <e0424709@u.nus.edu>
2021-01-06 03:32:52 +0000

    Fix timer when time is 1-9sec

--- Assets/Scripts/Level/CountdownTimer.cs
+++ Assets/Scripts/Level/CountdownTimer.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class CountdownTimer : MonoBehaviour
 {
     private float currentTime;
-    private float startingTime = 60f;
+    private float startingTime = 13;
 
     [SerializeField] TextMeshProUGUI countdownText;
 
@@ -19,7 +19,7 @@ public class CountdownTimer : MonoBehaviour
     {
         currentTime -= 1 * Time.deltaTime;
 
-        if (currentTime == 60)
+        if (currentTime == 60f)
         {
             countdownText.text = "01:00";
             return;
@@ -30,6 +30,13 @@ public class CountdownTimer : MonoBehaviour
             currentTime = 0;
         }
 
-        countdownText.text = "00:" + currentTime.ToString("0");
+        if (currentTime.ToString("0").Length == 1)
+        {
+            countdownText.text = "00:0" + currentTime.ToString("0");
+        }
+        else
+        {
+            countdownText.text = "00:" + currentTime.ToString("0");
+        }
     }
 }
