955a360f0ad45327ccec5f714d58eddf2427d367
Robin Ho <rhlb98@gmail.com>
2021-01-07 08:00:57 +0000

    bonus level made into slimes

---
+++ Assets/Scripts/Level/BouncyPeg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPeg : MonoBehaviour
+{
+    [SerializeField] private Animator pegController;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pegController.SetTrigger("SlimeBounce");
+        }
+    }
+}
