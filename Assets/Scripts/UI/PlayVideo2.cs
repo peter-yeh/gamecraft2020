abd5f29a1136e64ce6a59abb1f811d31f74659e0
Zhu Yuxuan <e0424709@u.nus.edu>
2021-01-08 14:14:26 +0000

    ANother way of loading video but it doesn;t seem to work either......

---
+++ Assets/Scripts/UI/PlayVideo2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class PlayVideo2 : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer vp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Blobtext2.mp4");
+        vp.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!vp.isPlaying)
+        {
+            Storage.GetStorage().setVideoPlayed();
+            SceneManager.LoadScene(3);
+        }
+    }
+}
